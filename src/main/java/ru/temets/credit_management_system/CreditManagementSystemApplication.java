8bb1c339b2a2aab5c4a4169f7b597f69ae7576ba
Artem Galoyan <rakkii7777@gmail.com>
2025-03-02 15:56:04 +0300

    Initial commit

---
+++ src/main/java/ru/temets/credit_management_system/CreditManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.temets.credit_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/temets/credit_management_system/CreditManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.temets.credit_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
